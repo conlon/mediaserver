36ab50bb52fe30f5e8b25eafe09ce101999c7b5e
Michael Conlon <michaelconlon0@gmail.com>
2018-02-20 00:36:47 -0600

    basic setup working - plex, deluge behind pia, sonarr, radarr, datadog - all in docker

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+  sudo apt autoremove -y
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y  ansible
+
+git clone https://github.com/conlon/mediaserver
+
+ansible-playbook mediaserver/main.yml -i mediaserver/inventories/localhost.inv -K
